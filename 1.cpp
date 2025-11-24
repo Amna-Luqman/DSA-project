acaf4335078bf5780c68faafa9319890424cd880
Amna-Luqman <63508@students.riphah.edu.pk>
2025-11-24 23:00:18 +0500

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,3 +1,23 @@
-#include <bits/stdc++.h>
-#include <windows.h>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <algorithm>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+#include <cctype>
 using namespace std;
+
+
+class Room {
+public:
+    string name;
+    string description;
+    bool locked;
+    vector<string> connections;
+
+    Room(string n="", string d="", bool l=false)
+        : name(n), description(d), locked(l) {}
+};
